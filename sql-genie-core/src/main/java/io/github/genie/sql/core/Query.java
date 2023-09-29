f9c8af3db915c77c52b4be6684433f8fea4c1e9d
Huang Chengwei <huang.chengwei@icloud.com>
2023-09-29 23:38:42 +0800

    cleanup code

--- sql-genie-core/src/main/java/io/github/genie/sql/core/Query.java
+++ sql-genie-core/src/main/java/io/github/genie/sql/core/Query.java
@@ -9,7 +9,6 @@ import java.util.Objects;
 import java.util.Optional;
 import java.util.function.Function;
 
-@SuppressWarnings("unused")
 public interface Query {
 
     <T> Select0<T, T> from(Class<T> type);
--- sql-genie-core/src/main/java/io/github/genie/sql/core/UnsafeTypeCast.java
+++
@@ -1,8 +0,0 @@
-package io.github.genie.sql.core;
-
-public class UnsafeTypeCast {
-    public static <T> T cast(Object o) {
-        // noinspection unchecked
-        return (T) o;
-    }
-}
